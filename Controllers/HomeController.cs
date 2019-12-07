7d5e755f5dda97e4a686f1bb75355af8c8f479c0
majeed13 <majeem@uw.edu>
2019-12-06 22:21:36 -0800

    Storing QuizResults in blob storage now added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.WindowsAzure.Storage.Table;
 using Newtonsoft.Json;
 using OnlineQuizApp.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -235,6 +238,26 @@ namespace OnlineQuizApp.Controllers
             Registration registration = (Registration)Session["SessionRegistration"];
             Student user = (Student)Session["SessionUser"];
 
+            // create the JSON MODEL to upload to blob storage
+            JsonModel blob = new JsonModel
+            {
+                name = user.name,
+                quizCategory = sQuiz.quizName,
+                finalScore = ans.correctAnswers.ToString() + " out of 10"
+            };
+            // fill questions list for JSON MODEL
+            for (int i = 0; i < q.Results.Length; i++)
+            {
+                QuestionModel question = new QuestionModel();
+                question.description = q.Results[i].Question;
+                question.choices = q.Results[i].IncorrectAnswers.ToList();
+                question.choices.Add(q.Results[i].CorrectAnswer);
+                question.correct = q.Results[i].CorrectAnswer;
+                blob.questions.Add(question);
+                blob.userChoice = ans.userAnswers[i];
+            }
+            uploadBlob(blob);
+            // call method to upload blob to storage container
             ViewBag.questionModel = q;
             ViewBag.ansList = ans.userAnswers;
             ViewBag.cat = sQuiz.quizName;
@@ -347,6 +370,27 @@ namespace OnlineQuizApp.Controllers
                 var val = list[k];
                 list[k] = list[n];
                 list[n] = val;
+            }
+        }
+
+        private void uploadBlob(JsonModel j)
+        {
+            // serialize the JSON MODEL object
+            var json = JsonConvert.SerializeObject(j);
+            // grab registration for token
+            Registration r = (Registration)Session["SessionRegistration"];
+            // create the clients needed to access blob
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            var blobContainer = blobClient.GetContainerReference("quizcontainer");
+            blobContainer.CreateIfNotExists();
+            // give a unique name to the blob
+            var newBlockBlob = blobContainer.GetBlockBlobReference(
+                "_" + r.token.ToString().Replace("-","_")
+                );
+            newBlockBlob.Properties.ContentType = "application/json";
+            using (MemoryStream ms = new MemoryStream(Encoding.ASCII.GetBytes(json)))
+            {
+                newBlockBlob.UploadFromStream(ms);
             }
         }
     }
---
+++ Models/JsonModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineQuizApp.Models
+{
+    public class QuestionModel
+    {
+        public string description { get; set; }
+        public List<string> choices { get; set; }
+        public string correct { get; set; }
+    }
+
+    public class JsonModel
+    {
+        public JsonModel()
+        {
+            questions = new List<QuestionModel>();
+        }
+        public string name { get; set; }
+        public string quizCategory { get; set; }
+        public List<QuestionModel> questions { get; set; }
+        public string userChoice { get; set; }
+        public string finalScore { get; set; }
+    }
+}
